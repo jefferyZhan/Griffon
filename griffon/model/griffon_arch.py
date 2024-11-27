d2401e940c039815841250f987e5b98bb628a48b
jefferyZhan <jeffery2000@126.com>
2024-11-27 12:08:34 +0800

    release Griffon-G inference

--- griffon/model/griffon_arch.py
+++ griffon/model/griffon_arch.py
@@ -1,4 +1,4 @@
-#    Copyright 2023 Yufei Zhan
+#    Copyright 2024 Yufei Zhan
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
